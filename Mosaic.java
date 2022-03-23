8afa4c4c4bdd9ead826108325657511a2c5d450b
Taylen LC <taylen@shaw.ca>
2022-03-22 22:43:07 -0700

    test image
    we love squidward

--- Mosaic.java
+++ Mosaic.java
@@ -13,9 +13,18 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 class Mosaic extends Frame { 
+	
+	BufferedImage test;
+	
+	int width;
+	int height;
 
 	public Mosaic() {
+		
+	
+		
 		try {
+			test = ImageIO.read(new File("squidward.jpg"));
 
 		} catch (Exception e) {
 			System.out.println("Cannot load the provided image");
@@ -31,18 +40,30 @@ class Mosaic extends Frame {
 					}
 				}
 				);
+		
+		width = test.getWidth();
+		height = test.getHeight();
 	}
 
 public void paint(Graphics g) {
-	this.setSize(600, 400);
+	this.setSize(850, 500);
 	
 	g.setColor(Color.BLACK);
-    Font f1 = new Font("Verdana", Font.PLAIN, 13);  
+    Font f1 = new Font("Consolas", Font.PLAIN, 13);  
     g.setFont(f1); 
     
-    g.drawString("hoi amena", 30, 70); 
-    g.drawString("henlo tay tay", 30, 120); 
-    g.drawString("{^0.0^} meow", 30, 160); 
+//    g.drawString("hoi amena", 30, 70); 
+//    g.drawString("henlo tay tay", 30, 120); 
+//    g.drawString("{^0.0^} meow", 30, 160); 
+    
+    g.drawString("hoi amena i'd like to share with you the best meme to state how the rest of the semester be feeling like",30,55); 
+    g.drawImage(test,70,100,width,height,this);
+    
+    g.setColor(Color.BLUE);
+    Font f2 = new Font("Comic Sans MS", Font.PLAIN, 13);  
+    g.setFont(f2); 
+    g.drawString("when herbert says we need study for the final midterm as well as prepare a report and presentation",70,450); 
+    
 }
 
 public static void main(String[] args) {
