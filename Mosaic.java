e2adf6022ab5879802e0a5c7107682c1131ed2db
Taylen LC <taylen@shaw.ca>
2022-04-10 22:30:36 -0700

    change title

--- Mosaic.java
+++ Mosaic.java
@@ -50,7 +50,7 @@ public class Mosaic extends JFrame{
 	}
 
 	public static void main(String[] args) {
-		new Mosaic("IAT 455 - Rocket Power");
+		new Mosaic("IAT 455 - Meta Meme");
 	}
 
 	public void selectFile() {
